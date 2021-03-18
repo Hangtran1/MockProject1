583e798a5cea8d0947aa7cbe8fb1313a505fa925
unknown <hangtran11.98@gmail.com>
2021-03-19 00:15:10 +0700

    initial code

---
+++ OTRequest.Core/Models/Project.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OTRequest.Core.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ProjectCode { get; set; }
+        [DisplayName("Project Name")]
+        public string ProjectName { get; set; }
+        [DisplayName("Start Date")]
+        public DateTime DateStart { get; set; }
+        [DisplayName("Start End")]
+        public DateTime DateEnd { get; set; }
+
+        public string ManagerId { get; set; }
+
+        public string QAId { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<UserProjectMap> TechnicalLeads { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<UserProjectMap> BA { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<UserProjectMap> Developers { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<UserProjectMap> Testers { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<UserProjectMap> TechnicalConsultancy { get; set; }
+
+        [ForeignKey("ProjectId")]
+        public virtual ICollection<Request> Requests { get; set; }
+    }
+}
